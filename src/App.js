666f088e0ecd3a3a085445276ecc318cb1ace6d5
prabhattripathii <pra.tri.27@gmail.com>
2023-01-18 23:39:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import HeaderComponent from './header';
+// import Input from '@mui/material/Input';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 function App() {
+
+  const [task, setTask] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (event) => {
+    setTask(event.target.value)
+  }
+
+  const newTaskToDo = () => {
+
+    const newTask = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: task
+    }
+
+    setTodoList([...todoList, newTask]);
+    console.log(todoList);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* Header Component */}
+      <HeaderComponent />
+      {/* Header Component */}
+      <br />
+      <Grid container justifyContent="center">
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: '100%',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <TextField fullWidth label="Enter Your Task" onChange={handleChange} id="fullWidth" />
+          <br />
+          <br />
+          <Button variant="contained" onClick={newTaskToDo}>ADD TASK</Button>
+        </Box>
+      </Grid>
+      {/* <div>
+        <input onChange={handleChange}/>
+        <button onClick={newTaskToDo}>Add Task</button>
+
+        {todoList.map((item)=>
+            <>
+            <h1>{item.id}</h1>
+            <h2>{item.taskName}</h2>
+            </>
+        )}
+      </div> */}
+      {todoList.map((item) =>
+        <>
+          <h1>{item.id}</h1>
+          <h2>{item.taskName}</h2>
+        </>
+      )}
     </div>
   );
 }
